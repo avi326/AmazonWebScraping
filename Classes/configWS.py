4ab3c36eb25294d0cba738d2821f9861aafa14f1
TalT <taltole@gmail.com>
2019-12-03 19:38:20 +0200

    more args updates

--- Classes/configWS.py
+++
@@ -1,9 +0,0 @@
-COUNTRY_IDX = 1
-LEAGUE_IDX = 2
-URL = 'https://www.scoreboard.com/en/soccer/'
-REQUIRED_NUM_OF_ARGS = 3
-ARG_OPTION = 1
-ARG_FILE_NAME = 2
-PRINT = -1
-
-'should add to .gitignore under *configWS.py'
